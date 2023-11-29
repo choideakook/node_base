771b797a36615909bb8f420f094d51dec1633ff9
choideakook <shdrnrhd113@naver.com>
2023-11-29 23:43:45 +0900

    feat: db 연동

--- my_app/index.js
+++ my_app/index.js
@@ -2,20 +2,41 @@ import {routes} from './routes/route.js';
 import {start} from './lib/figlet.js';
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
-const app = express(),
-    port = 3000;
+
+const app = express()
+const PORT = 3000;
+const MONGO_URL = 'mongodb://localhost:27017';
 
 //-- view setting --//
 app.set('view engine', 'pug')
 app.set('views', 'my_app/views')
 
+//-- Database --//
+mongoose.connect(MONGO_URL)
+    .then(() => console.log('DB 연결 성공'))
+    .catch(e => console.error(e));
+
+const {Schema} = mongoose;
+const WritingSchema = new Schema({
+    title: String,
+    contents: String,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Writing = mongoose.model('Writing', WritingSchema);
+
 //-- application setting --//
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     start('EXPRESS START')
-    console.log('Server is running on', port, 'port');
+    console.log('Server is running on', PORT, 'port');
 });
